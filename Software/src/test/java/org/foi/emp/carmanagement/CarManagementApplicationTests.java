d1e4324f6cdf299d52465fa663eb08cf198e7b3a
DESKTOP-J52P2N0\User <dposavec@foi.hr>
2021-05-07 23:14:54 +0200

    #1: Kreiran inicijalni projekt

---
+++ Software/src/test/java/org/foi/emp/carmanagement/CarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foi.emp.carmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
