e4434b3c49203a6e8d11c1729f10de96056f9715
DESKTOP-J52P2N0\User <dposavec@foi.hr>
2021-05-17 17:44:12 +0200

    #13 Kreirana klasa JwtRequest

---
+++ Software/src/main/java/org/foi/emp/carmanagement/models/JwtRequest.java
@@ -0,0 +1,17 @@
+package org.foi.emp.carmanagement.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtRequest implements Serializable {
+    private String username;
+    private String password;
+}
